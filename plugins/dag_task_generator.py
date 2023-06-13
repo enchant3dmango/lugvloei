9204f6a325acb831964bb3d1719abd0008171cfe
enchant3dmango <andre.yehuda@sirclo.com>
2023-06-13 22:18:47 +0700

    Test simple dynamic DAG generation
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation
    
    Update simple dynamic DAG generation

---
+++ plugins/dag_task_generator.py
@@ -0,0 +1,13 @@
+import os
+
+from airflow.decorators import task
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+
+from plugins.constants.miscellaneous import MYSQL_TO_BQ, POSTGRES_TO_BQ, RDBMS_TO_BQ, SUBMIT_SPARK_JOB
+
+
+def generate_task(dag_id, task_config):
+    if MYSQL_TO_BQ in RDBMS_TO_BQ.__members__ or POSTGRES_TO_BQ in RDBMS_TO_BQ.__members__:
+        return EmptyOperator(dag_id=dag_id, task_id=SUBMIT_SPARK_JOB)
