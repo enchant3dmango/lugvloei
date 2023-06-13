0ee7b72e121040a8e95003e6db6ebcc651b37d57
enchant3dmango <andre.yehuda@sirclo.com>
2023-06-13 23:00:44 +0700

    Update simple dynamic DAG generation

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -1,30 +1,40 @@
+from datetime import timedelta
 import os
+from airflow.decorators import dag, task
+
 import yaml
 
 from generators import mysql_to_bq, postgres_to_bq
 from plugins.utils.dag_config_reader import get_yaml_config_files
 from plugins.constants.variable import BRONZE, MYSQL_TO_BQ, POSTGRES_TO_BQ, SILVER
 
+
 config_files = get_yaml_config_files(os.getcwd(), '*.yaml')
 
+for config_file in config_files():
+    with open(config_file) as file:
+        config = yaml.safe_load(file)
+    if BRONZE in config.get('dag')['type']:
+        dag_id = f'{BRONZE}_{config.get("database")["name"]}.{config.get("database")["table"]}'
+    elif SILVER in config.get('dag')['type']:
+        dag_id = f'{SILVER}_{config.get("database")["name"]}.{config.get("database")["table"]}'
 
-def dynamic_dag(config, dag_id):
-    if MYSQL_TO_BQ in config.get('dag')['type']:
-        mysql_to_bq.generate_dag(config, dag_id)
-    elif POSTGRES_TO_BQ in config.get('dag')['type']:
-        postgres_to_bq.generate_dag(config, dag_id)
+    default_args = {
+        'owner': config.get('dag')['owner'],
+        'depend_on_past': config.get('dag')['depends_on_past'],
+        'email': config.get('dag')['email'],
+        'email_on_failure': config.get('dag')['email_on_failure'],
+        'email_on_retry': config.get('dag')['email_on_retry'],
+        'schedule_interval': config.get('dag')['schedule_interval'],
+        'concurrency': config.get('dag')['concurrency'],
+        'retries': config.get('dag')['retry']['count'],
+        'retry_delay': timedelta(minutes=config.get('dag')['retry']['delay']),
+    }
 
-
-def update_dynamic_dag():
-    for config_file in config_files():
-        with open(config_file) as file:
-            config = yaml.safe_load(file)
-        if BRONZE in config.get('dag')['type']:
-            dag_id = f'{BRONZE}_{config.get("database")["name"]}.{config.get("database")["table"]}'
-        elif SILVER in config.get('dag')['type']:
-            dag_id = f'{SILVER}_{config.get("database")["name"]}.{config.get("database")["table"]}'
-
-    dynamic_dag(config=config, dag_id=dag_id)
-
-
-update_dynamic_dag()
+    @dag(dag_id=dag_id, start_date=config.get('dag')['start_date'], default_args=default_args)
+    def dag_generator():
+        if MYSQL_TO_BQ in config.get('dag')['type']:
+            mysql_to_bq.generate_dag(config, dag_id)
+        elif POSTGRES_TO_BQ in config.get('dag')['type']:
+            postgres_to_bq.generate_dag(config, dag_id)
+    dag_generator()
--- generators/mysql_to_bq.py
+++ generators/mysql_to_bq.py
@@ -1,34 +1,10 @@
-from datetime import timedelta
+from airflow.decorators import task
 
-from airflow.decorators import dag, task
 
-class MySQLToBQ:
-    """
-        Class for MySQL to BQ DAG generator
-    """
-    
-    def __init__(self, config, dag_id) -> None:
-        self.config : list  = config
-        self.dag_id : str   = dag_id
-        
-        default_args = {
-            'owner'              : config.get('dag')['owner'],
-            'depend_on_past'     : config.get('dag')['depends_on_past'],
-            'email'              : config.get('dag')['email'],
-            'email_on_failure'   : config.get('dag')['email_on_failure'],
-            'email_on_retry'     : config.get('dag')['email_on_retry'],
-            'schedule_interval'  : config.get('dag')['schedule_interval'],
-            'concurrency'        : config.get('dag')['concurrency'],
-            'retries'            : config.get('dag')['retry']['count'],
-            'retry_delay'        : timedelta(minutes=config.get('dag')['retry']['delay']),
-        }
-    
-        @dag(dag_id=self.dag_id, start_date=self.config.get('dag')['start_date'], default_args=default_args)
-        def dynamic_generated_dag():
-            @task
-            def print_message(message):
-                print(message)
+def dynamic_generated_dag(config, dag_id):
+    @task
+    def print_message(message):
+        print(message)
 
-            print_message(config)
-
-        dynamic_generated_dag()
+    print_message(f'The config is {config}')
+    print_message(f'The dag_id is {dag_id}')
