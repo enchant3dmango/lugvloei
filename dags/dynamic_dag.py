8246251ad388960009eec4e9e9b96ac1e26a01c6
enchant3dmango <andre.yehuda@sirclo.com>
2023-06-16 17:33:13 +0700

    Update simple dynamic DAG generation

--- dags/dynamic_dag.py
+++ dags/dynamic_dag.py
@@ -1,63 +1,53 @@
 from __future__ import annotations
 
-from plugins.constants.miscellaneous import BRONZE, SILVER
+from plugins.constants.miscellaneous import RDBMS_TO_BQ
 from plugins.utils.dag_config_reader import get_yaml_config_files
 from datetime import timedelta
 import os
+
+from generators import rdbms_to_bq
 
 import yaml
 import pendulum
 
 from airflow.decorators import dag, task
 
-config_files = get_yaml_config_files(f'{os.environ["PYTHONPATH"]}/dags/configs', '*.yaml')
+config_files = get_yaml_config_files(
+    f'{os.environ["PYTHONPATH"]}/dags/configs', '*.yaml')
 
 for config_file in config_files:
     with open(config_file) as file:
         config = yaml.safe_load(file)
-    if BRONZE in config.get('dag')['type']:
-        dag_id = f'{BRONZE}_{config.get("database")["name"]}.{config.get("database")["table"]}'
-    elif SILVER in config.get('dag')['type']:
-        dag_id = f'{SILVER}_{config.get("database")["name"]}.{config.get("database")["table"]}'
 
+    dag_id = config.get('dag')['name']
     behavior = config.get('dag')['behavior']
 
     owner = config.get('dag')['owner']
-    email = config.get('dag')['email']
     depend_on_past = behavior['depends_on_past']
     start_date = tuple(map(int, behavior['start_date'].split(',')))
-    email_on_failure = behavior['email_on_failure']
-    email_on_retry = behavior['email_on_retry']
     schedule = behavior['schedule']
     catchup = behavior['catch_up']
-    max_active_tasks = behavior['max_active_tasks']
     retries = behavior['retry']['count']
-    retry_delay = timedelta(minutes=behavior['retry']['delay'])
+    retry_delay = timedelta(minutes=behavior['retry']['delay_in_minute'])
+
+    task_type = config.get('task')['type']
 
     default_args = {
         'owner': owner,
+        'email': ['data.engineer@sirclo.com'],
         'depend_on_past': depend_on_past,
-        'email': email,
-        'email_on_failure': email_on_failure,
-        'email_on_retry': email_on_retry,
         'retries': retries,
         'retry_delay': retry_delay,
         'catchup': catchup,
-        'max_active_tasks': max_active_tasks,
         'schedule': schedule
     }
 
     @dag(dag_id=dag_id, start_date=pendulum.datetime(*start_date, tz='Asia/Jakarta'), default_args=default_args)
-    def dynamic_generated_dag():
-            @task
-            def print_me(message):
-                print(message)
-
-            @task
-            def say_hello():
-                print('Hello')
-
-            print_me(config) >> say_hello()
-
+    def dynamic_generated_dag(task_type):
+        if task_type in RDBMS_TO_BQ:
+            task_flow = rdbms_to_bq.generate_task_flow()
+        # TODO: Add other task_type generator here
+        
+        # Add the task flow to dag 
+        task_flow
     dynamic_generated_dag()
-            
--- plugins/constants/miscellaneous.py
+++ plugins/constants/miscellaneous.py
@@ -1,5 +1,10 @@
+from enum import Enum
+
 MYSQL_TO_BQ = 'mysql_to_bq'
 POSTGRES_TO_BQ = 'postgres_to_bq'
 
 BRONZE = 'bronze'
 SILVER = 'silver'
+
+RDBMS_TO_BQ = Enum('RDBMS_TO_BQ', [MYSQL_TO_BQ, 
+                                   POSTGRES_TO_BQ])
