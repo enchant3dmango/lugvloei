d686868d0d8b0aa90dd6fc7367b4b0412a59779a
&re <andresitorusz.19@gmail.com>
2023-06-18 21:33:10 +0700

    Update simple dynamic DAG generation

--- dags/dynamic_dag.py
+++ dags/dynamic_dag.py
@@ -1,36 +1,35 @@
 from __future__ import annotations
 
-from plugins.constants.miscellaneous import RDBMS_TO_BQ
 from plugins.dag_config_reader import get_yaml_config_files
+from plugins.dag_task_generator import generate_task
 from datetime import timedelta
 import os
 
-from generators import rdbms_to_bq
 
 import yaml
 import pendulum
 
-from airflow.decorators import dag, task
+from airflow.decorators import dag
 
 config_files = get_yaml_config_files(
     f'{os.environ["PYTHONPATH"]}/dags/configs', '*.yaml')
 
 for config_file in config_files:
     with open(config_file) as file:
-        config = yaml.safe_load(file)
+        cfg = yaml.safe_load(file)
 
-    dag_id = config.get('dag')['name']
-    behavior = config.get('dag')['behavior']
+    dag_id = cfg.get('dag')['name']
+    dag_behavior = cfg.get('dag')['behavior']
 
-    owner = config.get('dag')['owner']
-    depend_on_past = behavior['depends_on_past']
-    start_date = tuple(map(int, behavior['start_date'].split(',')))
-    schedule = behavior['schedule']
-    catchup = behavior['catch_up']
-    retries = behavior['retry']['count']
-    retry_delay = timedelta(minutes=behavior['retry']['delay_in_minute'])
+    owner = cfg.get('dag')['owner']
+    depend_on_past = dag_behavior['depends_on_past']
+    start_date = tuple(map(int, dag_behavior['start_date'].split(',')))
+    schedule = dag_behavior['schedule']
+    catchup = dag_behavior['catch_up']
+    retries = dag_behavior['retry']['count']
+    retry_delay = timedelta(minutes=dag_behavior['retry']['delay_in_minute'])
 
-    task_type = config.get('task')['type']
+    task_type = cfg.get('task')['type']
 
     default_args = {
         'owner': owner,
@@ -43,17 +42,6 @@ for config_file in config_files:
     }
 
     @dag(dag_id=dag_id, start_date=pendulum.datetime(*start_date, tz='Asia/Jakarta'), default_args=default_args)
-    def dynamic_generated_dag(task_type):
-        # task_flow = None
-
-        if task_type in RDBMS_TO_BQ.__members__:
-            @task
-            def print_me(message):
-                print(message)
-            # task_flow = rdbms_to_bq.generate_task_flow()
-        # TODO: Add other task_type generator here
-
-        print_me(task_type)
-        # Add the task flow to dag
-        # task_flow
-    dynamic_generated_dag(task_type=task_type)
+    def dynamic_generated_dag(config):
+        generate_task(dag_id=dag_id, task_config=config)
+    dynamic_generated_dag(config=cfg.get('task'))
--- plugins/constants/miscellaneous.py
+++ plugins/constants/miscellaneous.py
@@ -1,10 +1,15 @@
 from enum import Enum
 
+# Task type
 MYSQL_TO_BQ = 'mysql_to_bq'
 POSTGRES_TO_BQ = 'postgres_to_bq'
 
+RDBMS_TO_BQ = Enum('RDBMS_TO_BQ', [MYSQL_TO_BQ,
+                                   POSTGRES_TO_BQ])
+
+# DAG level
 BRONZE = 'bronze'
 SILVER = 'silver'
 
-RDBMS_TO_BQ = Enum('RDBMS_TO_BQ', [MYSQL_TO_BQ, 
-                                   POSTGRES_TO_BQ])
+# Template
+SUBMIT_SPARK_JOB = 'submit_spark_job'
