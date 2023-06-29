360a7d4dc0637f57682b42c1c58c3c17ab577a97
&re <andresitorusz.19@gmail.com>
2023-06-30 00:51:16 +0700

    Remove trailing whitespaces

--- plugins/task_generator_set/rdbms_to_bq/rdbms_to_bq.py
+++ plugins/task_generator_set/rdbms_to_bq/rdbms_to_bq.py
@@ -173,8 +173,6 @@ class RdbmsToBq:
         application_args['jdbc_uri']          = self.__generate_jdbc_uri()
         application_args['type']              = self.task_type
         
-        
-        
         spark_kubernetes_operator_task =  SparkKubernetesOperator(
             task_id          = f'{SPARK_KUBERNETES_OPERATOR}_{self.dag_id}',
             application_file = f'{os.environ["PYTHONPATH"]}/dags/resources/rdbms_to_bq.yaml',
