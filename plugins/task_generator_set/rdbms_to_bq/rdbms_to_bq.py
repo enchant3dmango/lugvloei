840b3d342df540590ed635193ef142b6f7fd1a10
&re <andresitorusz.19@gmail.com>
2023-07-04 16:49:53 +0700

    Attempt to fix error

--- plugins/task_generator_set/rdbms_to_bq/rdbms_to_bq.py
+++ plugins/task_generator_set/rdbms_to_bq/rdbms_to_bq.py
@@ -176,7 +176,7 @@ class RdbmsToBq:
 
         spark_kubernetes_operator_task =  SparkKubernetesOperator(
             task_id            = f'{SPARK_KUBERNETES_OPERATOR}',
-            application_file   = f'{os.environ["PYTHONPATH"]}/resources/rdbms_to_bq.yaml ',
+            application_file   = 'resources/rdbms_to_bq.yaml',
             namespace          = 'airflow',
             in_cluster         = True,
             do_xcom_push       = True
