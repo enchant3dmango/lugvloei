3d19195a282f835beaac697559e9d709e7ba3d2c
&re <andresitorusz.19@gmail.com>
2023-07-04 11:22:15 +0700

    Attempt to fix error

--- plugins/task_generator_set/rdbms_to_bq/rdbms_to_bq.py
+++ plugins/task_generator_set/rdbms_to_bq/rdbms_to_bq.py
@@ -175,10 +175,11 @@ class RdbmsToBq:
 
         spark_kubernetes_operator_task =  SparkKubernetesOperator(
             task_id            = f'{SPARK_KUBERNETES_OPERATOR}',
-            application_file   = f'{os.environ["PYTHONPATH"]}/resources/rdbms_to_bq.yaml ',
+            application_file   = f'{os.environ["PYTHONPATH"]}/resources/rdbms_to_bq.yaml',
             namespace          = 'spark-operator',
             in_cluster         = True,
             do_xcom_push       = True,
+            kubernetes_conn_id = 'k8s',
             api_group          = 'sparkoperator.k8s.io',
             api_version        = 'v1beta2'
         )
