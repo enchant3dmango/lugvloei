c5b49e7f87b69acd628159aaf0f6ce3a93cd6cc9
&re <andresitorusz.19@gmail.com>
2023-07-05 13:58:50 +0700

    Test: add sleep

--- plugins/task_generator.py
+++ plugins/task_generator.py
@@ -1,3 +1,4 @@
+import time
 from plugins.constants.miscellaneous import (MYSQL_TO_BQ, POSTGRES_TO_BQ,
                                              RDBMS_TO_BQ)
 from plugins.task_generator_set.rdbms_to_bq.rdbms_to_bq import RdbmsToBq
@@ -9,4 +10,5 @@ def generate_task(dag_id, config):
 
     if MYSQL_TO_BQ in RDBMS_TO_BQ.__members__ or POSTGRES_TO_BQ in RDBMS_TO_BQ.__members__:
         rdbms_to_bq.generate_task()
+        time.sleep(30)
     # TODO: Add conditional statement for other task type here 
