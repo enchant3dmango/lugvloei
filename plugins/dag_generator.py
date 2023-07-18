0e7b86f664ecb18f8479b52af303acf2e6517777
&re <andresitorusz.19@gmail.com>
2023-07-18 19:14:38 +0700

    Add dag_generator feature flag

--- plugins/dag_generator.py
+++ plugins/dag_generator.py
@@ -7,43 +7,46 @@ import yaml
 from airflow.decorators import dag
 
 from plugins.constants.types import PYTHONPATH
+from plugins.constants.variables import DAG_GENERATOR_FEATURE_FLAG
 from plugins.task_generator import generate_task
 from plugins.utils.miscellaneous import get_dag_yaml_config_files
 
-config_files = get_dag_yaml_config_files(
-    f'{PYTHONPATH}/dags', '*.yaml')
+if DAG_GENERATOR_FEATURE_FLAG:
+    config_files = get_dag_yaml_config_files(
+        f'{PYTHONPATH}/dags', '*.yaml')
 
-for config_file in config_files:
-    with open(config_file) as file:
-        config = yaml.safe_load(file)
+    for config_file in config_files:
+        with open(config_file) as file:
+            config = yaml.safe_load(file)
 
-    dag_config = config.get('dag')
-    dag_id = dag_config['name']
-    dag_owner = dag_config['owner']
-    dag_tags = dag_config['tags']
-    dag_priority_weight = dag_config['priority_weight']
-    dag_behavior = dag_config['behavior']
+        dag_config = config.get('dag')
+        dag_id = dag_config['name']
+        dag_owner = dag_config['owner']
+        dag_tags = dag_config['tags']
+        dag_priority_weight = dag_config['priority_weight']
+        dag_behavior = dag_config['behavior']
 
-    # DAG behavior
-    depend_on_past = dag_behavior['depends_on_past']
-    start_date = tuple(map(int, dag_behavior['start_date'].split(',')))
-    schedule = dag_behavior['schedule']
-    catchup = dag_behavior['catch_up']
-    retries = dag_behavior['retry']['count']
-    retry_delay = timedelta(minutes=dag_behavior['retry']['delay_in_minute'])
+        # DAG behavior
+        depend_on_past = dag_behavior['depends_on_past']
+        start_date = tuple(map(int, dag_behavior['start_date'].split(',')))
+        schedule = dag_behavior['schedule']
+        catchup = dag_behavior['catch_up']
+        retries = dag_behavior['retry']['count']
+        retry_delay = timedelta(
+            minutes=dag_behavior['retry']['delay_in_minute'])
 
-    default_args = {
-        'owner': dag_owner,
-        'priority_weight': dag_priority_weight,
-        'email': ['data.engineer@sirclo.com'],
-        'depend_on_past': depend_on_past,
-        'retries': retries,
-        'retry_delay': retry_delay
-    }
+        default_args = {
+            'owner': dag_owner,
+            'priority_weight': dag_priority_weight,
+            'email': ['data.engineer@sirclo.com'],
+            'depend_on_past': depend_on_past,
+            'retries': retries,
+            'retry_delay': retry_delay
+        }
 
-    @dag(catchup=catchup, dag_id=dag_id, default_args=default_args, schedule=schedule,
-         start_date=pendulum.datetime(*start_date, tz='Asia/Jakarta'), tags=dag_tags,
-         template_searchpath=PYTHONPATH)
-    def generate_dag():
-        generate_task(dag_id=dag_id, config=config.get('task'))
-    generate_dag()
+        @dag(catchup=catchup, dag_id=dag_id, default_args=default_args, schedule=schedule,
+             start_date=pendulum.datetime(*start_date, tz='Asia/Jakarta'), tags=dag_tags,
+             template_searchpath=PYTHONPATH)
+        def generate_dag():
+            generate_task(dag_id=dag_id, config=config.get('task'))
+        generate_dag()
