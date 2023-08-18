423d1fb1b594c3a59ab39a0e1f98af6cf945ef5f
&re <andresitorusz.19@gmail.com>
2023-08-18 18:19:20 +0700

    Test: add owner into alert attachment

--- plugins/utilities/slack.py
+++ plugins/utilities/slack.py
@@ -6,6 +6,7 @@ from plugins.constants.connections import (SLACK_WEBHOOK_CONNECTION_ID,
 
 def generate_message(context):
     dag_id         = context.get('task_instance').dag_id
+    dag_owner      = context.get("dag").owner
     task_id        = context.get('task_instance').task_id
     log_url        = context.get('task_instance').log_url
     retry_count    = context.get('task_instance').try_number - 1
@@ -27,6 +28,11 @@ def generate_message(context):
                     {
                         "title": "Task ID:",
                         "value": f"_<{log_url}|{task_id}>_",
+                        "short": True
+                    },
+                    {
+                        "title": "DAG Owner:",
+                        "value": f"{dag_owner}",
                         "short": True
                     },
                     {
