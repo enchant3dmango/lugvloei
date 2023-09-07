6f0da5db4fb1177af4e87357bdd5fc5c58e0ec33
&re <andresitorusz.19@gmail.com>
2023-09-07 14:50:53 +0700

    Rename GCP_CONN_ID variable

--- plugins/constants/variables.py
+++ plugins/constants/variables.py
@@ -12,7 +12,7 @@ if int(Variable.get('DAG_GENERATOR_FEATURE_FLAG')) in [0, 1]:
 else:
     raise ValueError("DAG_GENERATOR_FEATURE_FLAG must be 0 or 1.")
 
-GCP_CONN_ID = Variable.get('gcp', None)
+GCP_CONN_ID = Variable.get('GCP_CONN_ID', None)
 
 # Length to retain the log files if not already provided in the conf. If this
 # is set to 30, the job will remove those files that are 30 days old or older.
