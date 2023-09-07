8f67ca6f0345c4b87bc0df692a55caa0cfea8e90
&re <andresitorusz.19@gmail.com>
2023-09-07 22:13:17 +0700

    Revert "Attempt to fix error"
    This reverts commit eefa3617a0851ac105084260aefc6acff3ab4477.

--- plugins/task_generators/rdbms_to_bq/rdbms_to_bq.py
+++ plugins/task_generators/rdbms_to_bq/rdbms_to_bq.py
@@ -302,13 +302,11 @@ class RDBMSToBQGenerator:
             elif type(self.source_connection) is list:
                 for index, connection in enumerate(sorted(self.source_connection)):
                     filename = f'{self.target_bq_dataset}/{self.target_bq_table}/{iso8601_date}/{self.source_table}_{index}_*.json'
-
                     extract = []
-                    __extract = f'{extract}_{index}'
 
                     # Extract data from Postgres, then load to GCS
                     if self.task_type == POSTGRES_TO_BQ:
-                        globals()[__extract] = PostgresToGCSOperator(
+                        __extract = PostgresToGCSOperator(
                             task_id          = f'extract__{self.source_table}_{index}',
                             postgres_conn_id = connection,
                             gcp_conn_id      = GCP_CONN_ID,
@@ -322,7 +320,7 @@ class RDBMSToBQGenerator:
 
                     # Extract data from MySQL, then load to GCS
                     elif self.task_type == MYSQL_TO_BQ:
-                        globals()[__extract] = MySQLToGCSOperator(
+                        __extract = MySQLToGCSOperator(
                             task_id        = f'extract__{self.source_table}_{index}',
                             mysql_conn_id  = connection,
                             gcp_conn_id    = GCP_CONN_ID,
