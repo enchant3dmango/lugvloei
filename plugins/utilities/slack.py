363684410fbeea95a1f929f3ad1097560b4dfcdb
&re <andresitorusz.19@gmail.com>
2023-09-20 21:04:51 +0700

    Add on_success_callback

--- plugins/utilities/slack.py
+++ plugins/utilities/slack.py
@@ -4,13 +4,13 @@ from plugins.constants.connections import SLACK_WEBHOOK_CONNECTION_ID
 
 
 def generate_failure_message(context):
-    dag_id         = context.get('task_instance').dag_id
-    dag_owner      = context.get("dag").owner
-    task_id        = context.get('task_instance').task_id
-    log_url        = context.get('task_instance').log_url
-    retry_count    = context.get('task_instance').try_number - 1
-    run_id         = context.get('task_instance').run_id
-    error_message  = (str(context['exception'])[:140] + '...') if len(
+    dag_id        = context.get('task_instance').dag_id
+    dag_owner     = context.get("dag").owner
+    task_id       = context.get('task_instance').task_id
+    log_url       = context.get('task_instance').log_url
+    retry_count   = context.get('task_instance').try_number - 1
+    run_id        = context.get('task_instance').run_id
+    error_message = (str(context['exception'])[:140] + '...') if len(
         str(context['exception'])) > 140 else str(context['exception'])
 
     return {
@@ -51,9 +51,42 @@ def generate_failure_message(context):
     }
 
 
+def generate_success_message(context):
+    dag_id    = context.get('task_instance').dag_id
+    dag_owner = context.get("dag").owner
+    run_id    = context.get('task_instance').run_id
+
+    return {
+        "text": ":alert_green: *Task Success Notification!*",
+        "attachments": [
+            {
+                "color": "#2ECC40",
+                "fields": [
+                    {
+                        "title": "DAG ID:",
+                        "value": f"{dag_id}",
+                        "short": True
+                    },
+                    {
+                        "title": "DAG Owner:",
+                        "value": f"{dag_owner}",
+                        "short": True
+                    },
+                    {
+                        "title": "Message:",
+                        "value": "Flawless execution! Your DAG has successfully completed its tasks.",
+                        "short": False
+                    }
+                ],
+                "footer": f"Run ID: {run_id}",
+            }
+        ]
+    }
+
+
 def on_failure_callback(context):
     """ 
-    Send alert into Slack alert channel everytime a DAG failed.
+    Send alert into Slack channel everytime a DAG failed.
     """
 
     operator = SlackWebhookOperator(
@@ -61,6 +94,21 @@ def on_failure_callback(context):
         slack_webhook_conn_id = SLACK_WEBHOOK_CONNECTION_ID,
         message               = generate_failure_message(context=context)['text'],
         attachments           = generate_failure_message(context=context)['attachments']
+    )
+
+    return operator.execute(context=context)
+
+
+def on_success_callback(context):
+    """ 
+    Send notification into Slack channel everytime a DAG success.
+    """
+
+    operator = SlackWebhookOperator(
+        task_id               = 'on_success_callback',
+        slack_webhook_conn_id = SLACK_WEBHOOK_CONNECTION_ID,
+        message               = generate_success_message(context=context)['text'],
+        attachments           = generate_success_message(context=context)['attachments']
     )
 
     return operator.execute(context=context)
