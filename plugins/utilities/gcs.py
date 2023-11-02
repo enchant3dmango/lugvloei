88c697d4b1557853876206562ea1efa889a4fe3e
&re <andresitorusz.19@gmail.com>
2023-11-02 16:31:56 +0700

    Fix file path

--- plugins/utilities/gcs.py
+++ plugins/utilities/gcs.py
@@ -19,10 +19,10 @@ def upload_multiple_files_from_local(bucket: str, dirname: str = None, privacy: 
     # Upload files to GCS
     for file in os.listdir(local_path):
         # The name of file on GCS once uploaded
-        blob = gcs_bucket.blob(file)
-        logging.info(f'Uploading {file} to {blob}.')
+        blob = gcs_bucket.blob(f'{local_path}/{file}')
+        logging.info(f'Uploading {local_path}/{file} to {blob}.')
         # Path of the local file to upload
-        blob.upload_from_filename(file)
+        blob.upload_from_filename(f'{local_path}/{file}')
 
     logging.info(f"Successfully uploaded all files in {os.path.dirname(local_path)} uploaded to GCS.")
 
