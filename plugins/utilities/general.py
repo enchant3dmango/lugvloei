a02f7d9cdd05d381dcab582b7dd4db73944c8c17
&re <andresitorusz.19@gmail.com>
2023-11-14 15:08:54 +0700

    Test

--- plugins/utilities/general.py
+++ plugins/utilities/general.py
@@ -162,12 +162,7 @@ def polars_dataframe_type_casting(dataframe: pl.DataFrame, schema: list, **kwarg
         if field_type == "DATE" and isinstance(format_date, str):
             dataframe = dataframe.with_columns(pl.col(field_name).cast(dtype=pl.Date, strict=False))
         elif field_type == "TIMESTAMP" and (format_timestamp is None or isinstance(format_timestamp, str)):
-            dataframe = dataframe.with_columns(
-                pl.col(field_name).str.strptime(
-                    pl.Datetime,
-                    strict=False
-                )
-            )
+            dataframe = dataframe.with_columns(pl.col(field_name).cast(dtype=pl.Datetime, strict=False))
         elif field_type == "TIME":
             dataframe = dataframe.with_columns(pl.col(field_name).cast(dtype=pl.Time, strict=False))
         elif field_type == "FLOAT":
