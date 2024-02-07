0dcc0465b40f010df23d1f4b0b229caffe186360
&re <andresitorusz.19@gmail.com>
2024-02-07 15:55:25 +0700

    fix: remove unused variables

--- plugins/constants/variables.py
+++ plugins/constants/variables.py
@@ -14,10 +14,4 @@ else:
 
 GCP_CONN_ID = Variable.get('GCP_CONN_ID', None)
 
-# Length to retain the log files if not already provided in the conf. If this
-# is set to 30, the job will remove those files that are 30 days old or older.
-DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS = int(
-    Variable.get("airflow_db_cleanup__max_db_entry_age_in_days", 30)
-)
-
 GCS_DATA_LAKE_BUCKET = Variable.get('GCS_DATA_LAKE_BUCKET', None)
