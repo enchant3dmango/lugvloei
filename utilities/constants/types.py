bb5f0d177b977d7210e9a372025b4345a0c130ad
enchant3dmango <andresitorusz.19@gmail.com>
2025-02-03 13:52:14 +0700

    chore: remove unused var

--- utilities/constants/types.py
+++ utilities/constants/types.py
@@ -3,7 +3,6 @@ import os
 # Environment and miscellaneous variables
 AIRFLOW = "airflow"
 AIRFLOW_HOME = os.environ["AIRFLOW_HOME"]
-DATABASE = "database"
 DE_DAG_OWNER_NAME = "owner@email.com"
 PYTHONPATH = os.environ["PYTHONPATH"]
 SPARK = "spark"
