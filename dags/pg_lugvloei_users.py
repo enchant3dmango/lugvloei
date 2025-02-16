5aa30cd9afb05da62618581b4c7ce71ad0e73d6a
enchant3dmango <andresitorusz.19@gmail.com>
2025-02-16 22:14:38 +0700

    style: remove redundant vars

--- dags/pg_lugvloei_users.py
+++ dags/pg_lugvloei_users.py
@@ -4,12 +4,12 @@ from datetime import timedelta
 
 import pendulum
 from airflow.decorators import dag
-from airflow.models import Variable
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
 from google.cloud.bigquery import DestinationFormat, SourceFormat, WriteDisposition
 
 from utilities.constants.types import DE_DAG_OWNER_NAME
+from utilities.constants.variables import GCP_CONN_ID, GCS_DATA_LAKE_BUCKET
 from utilities.slack import on_failure_callback, on_success_callback
 
 start_date = pendulum.datetime(2025, 1, 25, tz="Asia/Jakarta")
@@ -36,8 +36,6 @@ default_args = {
     schedule="@daily",
 )
 def generate_dag():
-    GCS_DATA_LAKE_BUCKET = Variable.get("GCS_DATA_LAKE_BUCKET", None)
-    GCP_CONN_ID = Variable.get("GCP_CONN_ID", "google_cloud_default")
     ts_nodash = "{{ data_interval_start.astimezone(dag.timezone).strftime('%Y-%m-%d %H:%M:%S') }}"
     filename = f"lugvloei/users/{ts_nodash}/users"
 
--- utilities/constants/variables.py
+++ utilities/constants/variables.py
@@ -1,5 +1,5 @@
 from airflow.models import Variable
 
 SPARK_JOB_NAMESPACE = Variable.get('SPARK_JOB_NAMESPACE', None)
-GCP_CONN_ID = Variable.get('GCP_CONN_ID', None)
+GCP_CONN_ID = Variable.get('GCP_CONN_ID', 'google_cloud_default')
 GCS_DATA_LAKE_BUCKET = Variable.get('GCS_DATA_LAKE_BUCKET', None)
